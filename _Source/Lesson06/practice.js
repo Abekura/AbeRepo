822a71fcd13a81898a60636758535c6b7a6ad4f1
Abekura <abekura0226@gmail.com>
2021-11-18 12:08:47 +0900

    練習問題解答済み

---
+++ _Source/Lesson06/practice.js
@@ -0,0 +1,39 @@
+function pra1(){
+    var text = document.getElementById("pra1Text").value
+    alert(text);
+};
+
+function pra2(){
+    var selectedVal = document.getElementById("selectBox01").value
+    if(selectedVal == "○"){
+        alert("OK");
+    } else {
+        //elseの場合は処理無し。
+    }
+};
+
+function pra3(){
+    var pra3Val1 = document.getElementById("pra3Tag1").value
+    var pra3Val2 = document.getElementById("pra3Tag2").value
+    var pra3Val3 = document.getElementById("pra3Tag3").value
+    if(pra3Val1 == "×" && pra3Val2 == "×" && pra3Val3 == "×"){
+        alert("OK");
+    } else {
+        //elseの場合は処理無し。
+    }
+};
+
+function pra4(){
+    var pra4Val1 = document.getElementById("pra4Tag1").value
+    var pra4Val2 = document.getElementById("pra4Tag2").value
+    var pra4Val3 = document.getElementById("pra4Tag3").value
+    var pra4Val4 = document.getElementById("pra4Tag4").value
+    if(pra4Val1 == "○" && pra4Val4 == "○" && pra4Val2 == "×" && pra4Val3 == "×"){
+        alert("OK");
+    } else {
+        if(pra4Val2 == "○" && pra4Val3 == "○" && pra4Val1 == "×" && pra4Val4 == "×") {
+            alert("OK");
+        }
+    }
+    
+};
